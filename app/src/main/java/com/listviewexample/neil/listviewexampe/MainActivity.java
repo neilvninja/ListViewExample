b559a42a39e499c7695f7ece9333c97af24686fe
neil <neilvninja@yahoo.co.uk>
2014-07-13 00:04:21 +0100

    Initial commit

---
+++ app/src/main/java/com/listviewexample/neil/listviewexampe/MainActivity.java
@@ -0,0 +1,57 @@
+package com.listviewexample.neil.listviewexampe;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] bestCards = {"Bee", "Brown Wynn", "Aviator", "WPT", "Jerry's Nuggets", "Bicycle", "Generic", "Theory eleven", "Tally Ho"};
+
+        ListAdapter theAdapter = new MyAdapter(this, bestCards);
+
+        ListView theListView = (ListView) findViewById(R.id.theListView);
+        theListView.setAdapter(theAdapter);
+
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String cardBrandPicked = "You chose " + String.valueOf(adapterView.getItemAtPosition(position));
+                Toast.makeText(MainActivity.this, cardBrandPicked, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/listviewexample/neil/listviewexampe/MyAdapter.java
@@ -0,0 +1,32 @@
+package com.listviewexample.neil.listviewexampe;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+class MyAdapter extends ArrayAdapter<String> {
+
+    public MyAdapter(Context context, String[] values) {
+        super(context, R.layout.row_layout_2, values);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View theView = inflater.inflate(R.layout.row_layout_2, parent, false);
+
+        String cardBrand = getItem(position);
+        TextView theTextView = (TextView) theView.findViewById(R.id.textView1);
+
+        theTextView.setText(cardBrand);
+
+        ImageView theImageView = (ImageView) theView.findViewById(R.id.imageView1);
+        theImageView.setImageResource(R.drawable.dot);
+
+        return theView;
+    }
+}
